aa3618faea470f308cee11749312c13011a2c97c
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2012-08-16 17:52:18 +0400

    fixed bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -10,11 +10,6 @@
  * file that was distributed with this source code.
  */
 
-if ((!$loader = @include __DIR__.'/../../../.composer/autoload.php') && (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php')) {
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
-}
-
+$loader = @include __DIR__.'/../vendor/autoload.php';
 $loader->add('test', __DIR__ . "/../");
 $loader->add('Stub', __DIR__);
